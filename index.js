d56ac08bd9d7ee404e79b33609997f0d8f8a4d03
Paulina Kaczmarek <kaczmarekpaulina@yahoo.com>
2025-02-01 22:27:56 +1100

    new-changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// DEscription I want to :
+// 1.Add product to Cart with name and price
+// 2.sum price to give final price
+// 3.Restart btn
+// Login
+// 1.do check of Input
+// 2.password
 
+const addButton = document.querySelector("add-item");
+
+function open() {
+  alert("add");
+}
+
+addButton.addEventListener("click", open);
