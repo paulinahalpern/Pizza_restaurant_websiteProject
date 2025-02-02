5f3f94beb922adaa8c8af03afc0d070229981fb8
Paulina Kaczmarek <kaczmarekpaulina@yahoo.com>
2025-02-02 22:05:17 +1100

    changes

--- index.js
+++ index.js
@@ -9,17 +9,26 @@
 const addToCartBtn = document.querySelectorAll(".add-item");
 const cartItems = document.querySelector("#cart-content");
 const productNames = document.querySelectorAll(".top-half");
-const productPrice = document.querySelectorAll(".bottom-half");
+const productPrices = document.querySelectorAll(".bottom-half");
 const shoppingList = document.querySelector("cart-items");
 const restartBtn = document.querySelector(".btn-restart");
+const sumValue = document.getElementById("summary-value");
 
+let priceTotal = 0;
 addToCartBtn.forEach((element, index) => {
   element.onclick = function () {
     const sentence = document.querySelector(".add-products");
     let cartItem = document.createElement("li");
+    let cartPrice = document.createElement("div");
+    cartPrice.className = "cart-price";
     let itemName = productNames[index].textContent;
+    let itemPrice = productPrices[index].textContent;
     cartItem.textContent = itemName;
+    cartPrice.textContent = itemPrice;
     cartItems.appendChild(cartItem);
+    cartItems.appendChild(cartPrice);
+    priceTotal += itemPrice;
+    // sumValue.innerHTML = ${`priceTotal`};
     sentence.style.display = "none";
   };
   restartBtn.onclick = function () {
