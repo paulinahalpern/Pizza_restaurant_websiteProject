642ac9a23ebb45f54a2f09e66d0d41138582c858
Paulina Halpern <kaczmarekpaulina@yahoo.com>
2025-02-11 22:21:11 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// DEscription I want to :
-// 1.Add product to Cart with name and price
-// 2.sum price to give final price
-// 3.Restart btn
-// Login
-// 1.do check of Input
-// 2.password
-
 const addToCartBtn = document.querySelectorAll(".add-item");
 const cartItems = document.querySelector("#cart-content");
 const productNames = document.querySelectorAll(".top-half");
