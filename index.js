e7e8014829b50db695d505d6e29d0353b00b1b75
Paulina Kaczmarek <paulina.halpern@yahoo.com>
2025-02-11 22:03:00 +1100

    Final_changes

--- index.js
+++ index.js
@@ -10,32 +10,53 @@ const addToCartBtn = document.querySelectorAll(".add-item");
 const cartItems = document.querySelector("#cart-content");
 const productNames = document.querySelectorAll(".top-half");
 const productPrices = document.querySelectorAll(".bottom-half");
-const shoppingList = document.querySelector("cart-items");
 const restartBtn = document.querySelector(".btn-restart");
 const sumValue = document.getElementById("summary-value");
+const itemCountDisplay=document.querySelector('.item-count')
 
 let priceTotal = 0;
+let itemCount=0;
+
 addToCartBtn.forEach((element, index) => {
   element.onclick = function () {
     const sentence = document.querySelector(".add-products");
-    let cartItem = document.createElement("li");
-    let cartPrice = document.createElement("div");
-    cartPrice.className = "cart-price";
-    let itemName = productNames[index].textContent;
-    let itemPrice = productPrices[index].textContent;
-    cartItem.textContent = itemName;
-    cartPrice.textContent = itemPrice;
-    cartItems.appendChild(cartItem);
-    cartItems.appendChild(cartPrice);
-    priceTotal += itemPrice;
     
-    sentence.style.display = "none";
+    let cartItemContainer = document.createElement("li");
+    cartItemContainer.className = "cart-item"; 
+    
+    let cartItem = document.createElement("span");
+    let cartPrice = document.createElement("span");
+    cartPrice.className = "cart-price";
+
+    let itemName = productNames[index].textContent;
+    let itemPrice = parseFloat(productPrices[index].textContent.replace(/[^0-9.]/g, "")); 
+    // This finds the first number, and replaces the entire string with the matched number.
+
+
+
+    cartItem.textContent = itemName;
+    cartPrice.textContent = `$${itemPrice.toFixed(2)}`;
+    // toFixed method converts a number to a string.
+
+    cartItemContainer.appendChild(cartItem);
+    cartItemContainer.appendChild(cartPrice);
+    
+    cartItems.appendChild(cartItemContainer);
+
+    priceTotal += itemPrice;
+    itemCount++;
+    sumValue.textContent = `$${priceTotal.toFixed(2)}`;
+    itemCountDisplay.textContent = `Items: ${itemCount}`
   };
   restartBtn.onclick = function () {
     cartItems.innerHTML = "";
-    sentence.style.display = "visible";
+    priceTotal = 0;
+    itemCount= 0;
+    sumValue.textContent = "$0.00";
   };
+  
 });
+
 
 // Login 
 
@@ -52,24 +73,21 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!loginName.value.trim()){
     messages.push('Name is required')
   }
-  if (loginName.value ==='' || loginName.value== null){
-    messages.push('Name is required');
-  }
+  
   if (password.value.length< 13){
     messages.push('Password must be longer than 13 characters. ');
-  }
-  if (password.value.length>= 20){
+  }else if (password.value.length >= 20){
     messages.push('Password must be less than 20 characters. ');
-  }
-  if (password.value==='password'){
+  }else if(password.value==='password'){
     messages.push('Password cannot be password');
   }
   if (messages.length>0){
     e.preventDefault();
-    errorElement.innerText= messages.join(',')
+    errorElement.innerText= messages.join(',<br>')
   } 
 })
 });
+
 
 function openLoginView(){
   const loginForm = document.getElementById('form');
@@ -95,9 +113,9 @@ function openHamburgerMenu() {
 document.addEventListener('DOMContentLoaded', function(){
   const hamburgerMenu= document.querySelector('#menu-trigger');
   if (hamburgerMenu){
-    hamburgerMenu.addEventListener('click',openHamburgerMenu)
+    hamburgerMenu.addEventListener('click',openHamburgerMenu);
   }
-})
+});
 
 
 // Shopping Cart Icon
@@ -110,4 +128,4 @@ function openShopCart(){
 document.addEventListener('DOMContentLoaded', function(){
 const btnShopCart= document.getElementById('btn-shop-cart')
 btnShopCart.addEventListener('click',openShopCart);
-})
+});
